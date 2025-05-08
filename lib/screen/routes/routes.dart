662fbd06a235f45a23b1e7716f96afbe610ee06c
Harsya1 <dika20949@gmail.com>
2025-05-08 11:28:54 +0700

    feat: update dashboard layout; integrate room list and calendar view

--- lib/screen/routes/routes.dart
+++ lib/screen/routes/routes.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import '../dashboard_calender.dart';
-import '../dashboard_camp.dart';
+import '../dashboard_calender.dart' as calender; // Alias untuk dashboard_calender.dart
+import '../dashboard_camp.dart' as campt; // Alias untuk dashboard_camp.dart
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
-    // Jika ingin mengirim argument
-    // final args = settings.arguments;
     switch (settings.name) {
       case '/dashboard_calender':
-        return MaterialPageRoute(builder: (_) => const DashboardCalender());
+        return MaterialPageRoute(builder: (_) => const calender.DashboardCalender());
       case '/dashboard_camp':
-        return MaterialPageRoute(builder: (_) => const DashboardCamp());
+        return MaterialPageRoute(builder: (_) => const campt.DashboardCamp());
       default:
         return _errorRoute();
     }
